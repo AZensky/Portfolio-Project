9d6944763eed7a1fbb9bcdee648de8dc1c715150
alex <alexzelinsky124@gmail.com>
2022-09-14 10:10:59 -0700

    project links added

--- src/section/Portfolio.jsx
+++ src/section/Portfolio.jsx
@@ -1,8 +1,8 @@
-import { Box, Button, Container, Divider, Typography } from '@mui/material';
-import React from 'react';
-import { portfolio } from '../assets';
-import PortfolioTabs from '../components/PortfolioTab/PortfolioTab';
-import SliderView from '../components/Slider/Slider';
+import { Box, Button, Container, Divider, Typography } from "@mui/material";
+import React from "react";
+import { portfolio } from "../assets";
+import PortfolioTabs from "../components/PortfolioTab/PortfolioTab";
+import SliderView from "../components/Slider/Slider";
 
 export default function Portfolio() {
   return (
@@ -11,25 +11,32 @@ export default function Portfolio() {
         background: (theme) => theme.palette.primary.dark,
         py: 16,
         zIndex: 9,
-        position: 'relative',
+        position: "relative",
         px: { md: 2 },
-      }}>
-      <Container maxWidth={false} sx={{ maxWidth: 1610, textAlign: 'center' }}>
+      }}
+    >
+      <Container maxWidth={false} sx={{ maxWidth: 1610, textAlign: "center" }}>
         <Box
           sx={{
             background: `url(${portfolio})`,
-            backgroundRepeat: 'no-repeat',
+            backgroundRepeat: "no-repeat",
             mb: 5,
-            textAlign: 'center',
-          }}>
+            textAlign: "center",
+          }}
+        >
           <Typography
-            variant='h4'
+            variant="h4"
             sx={{
-              textTransform: 'capitalize',
-            }}>
+              textTransform: "capitalize",
+            }}
+          >
             Creative
-            <Typography variant='h4' component='span' sx={{ color: (theme) => theme.palette.primary.main }}>
-              {' '}
+            <Typography
+              variant="h4"
+              component="span"
+              sx={{ color: (theme) => theme.palette.primary.main }}
+            >
+              {" "}
               Portfolio
             </Typography>
           </Typography>
@@ -37,9 +44,9 @@ export default function Portfolio() {
           <Divider
             sx={{
               height: 2,
-              background: '#fff',
+              background: "#fff",
               width: 105,
-              mx: 'auto',
+              mx: "auto",
               mt: 1,
               mb: 3,
             }}
@@ -49,19 +56,20 @@ export default function Portfolio() {
           }
           <SliderView />
           <Button
-            variant='outlined'
+            variant="outlined"
             sx={{
               width: 180,
               mt: 5,
               fontSize: 20,
-              mx: 'auto',
-              '&:hover': {
+              mx: "auto",
+              "&:hover": {
                 background: (theme) => theme.palette.primary.main,
-                color: '#fff',
-                fontWeight: 'bold',
+                color: "#fff",
+                fontWeight: "bold",
               },
-            }}>
-            Heir Me
+            }}
+          >
+            Contact Me
           </Button>
         </Box>
       </Container>
