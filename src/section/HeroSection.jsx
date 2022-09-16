b84fe2a8ab3e54d62ead669211a2914e358d7558
alex <alexzelinsky124@gmail.com>
2022-09-16 16:14:17 -0700

    adjusted letter spacing for portfolio

--- src/section/HeroSection.jsx
+++ src/section/HeroSection.jsx
@@ -1,41 +1,57 @@
-import { Box, Button, Typography } from '@mui/material';
-import React from 'react';
-import Typed from 'react-typed';
-import { headerBg } from '../assets';
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import Typed from "react-typed";
+import { headerBg } from "../assets";
 
 export default function HeroSection({ handleBackClick }) {
   return (
     <Box
       sx={{
         background: `url(${headerBg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: '100%',
+        backgroundSize: "cover",
+        backgroundPosition: "100%",
         // height: { xs: 560, md: 840 },
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        mt:  -12 ,
-      }}>
-      <Box sx={{ textAlign: 'center' }}>
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        mt: -12,
+      }}
+    >
+      <Box sx={{ textAlign: "center" }}>
         <Typography
-          variant='h1'
+          variant="h1"
           sx={{
-            letterSpacing: { xs: '0.20em', lg: '0.22em' },
+            letterSpacing: { xs: "0.16em", lg: "0.18em" },
 
-            textTransform: 'uppercase',
-          }}>
+            textTransform: "uppercase",
+          }}
+        >
           Alex
-          <Typography variant='h1' component='span' sx={{ color: (theme) => theme.palette.primary.main }}>
-            {' '}
-            Zelinsky{' '}
+          <Typography
+            variant="h1"
+            component="span"
+            sx={{
+              color: (theme) => theme.palette.primary.main,
+              letterSpacing: { xs: "0.16em", lg: "0.18em" },
+            }}
+          >
+            {" "}
+            Zelinsky{" "}
           </Typography>
         </Typography>
-        <Typography variant='h3' sx={{ color: (theme) => theme.palette.primary.light, my: 2.5 }}>
+        <Typography
+          variant="h3"
+          sx={{ color: (theme) => theme.palette.primary.light, my: 2.5 }}
+        >
           {/* Creative{' '} */}
-          <Typography component='span' variant='h3' sx={{ color: '#fff' }}>
+          <Typography component="span" variant="h3" sx={{ color: "#fff" }}>
             <Typed
-              strings={['Software Engineer', 'Web Developer', 'Technical Writer']}
+              strings={[
+                "Software Engineer",
+                "Web Developer",
+                "Technical Writer",
+              ]}
               typeSpeed={80}
               backDelay={1100}
               backSpeed={60}
@@ -56,14 +72,15 @@ export default function HeroSection({ handleBackClick }) {
           // </Typography>
         }
         <Button
-          variant='contained'
+          variant="contained"
           sx={{
             fontSize: { xs: 16, md: 18, xl: 20 },
-            justifySelf: 'center',
+            justifySelf: "center",
             mt: { md: 1, lg: 2 },
             background: (theme) => theme.palette.primary.main,
           }}
-          onClick={() => handleBackClick('contact')}>
+          onClick={() => handleBackClick("contact")}
+        >
           Get In Touch
         </Button>
       </Box>
