fbb1e6a38c6a31905c4448f6c7bb2918b45ba12b
alex <alexzelinsky124@gmail.com>
2023-01-20 08:17:39 -0800

    changed contact description and copyright

--- src/section/GetInTouch.jsx
+++ src/section/GetInTouch.jsx
@@ -1,7 +1,7 @@
-import { Box, Container, Divider, Typography } from '@mui/material';
-import React from 'react';
-import { getInTouch } from '../assets';
-import GetInTouchForm from '../components/GetInTouchForm';
+import { Box, Container, Divider, Typography } from "@mui/material";
+import React from "react";
+import { getInTouch } from "../assets";
+import GetInTouchForm from "../components/GetInTouchForm";
 
 export default function GetInTouch() {
   return (
@@ -10,26 +10,33 @@ export default function GetInTouch() {
         background: (theme) => theme.palette.primary.dark,
         pt: 16,
         zIndex: 9,
-        position: 'relative',
+        position: "relative",
         px: { md: 2 },
         pb: 6.5,
-      }}>
-      <Container maxWidth='md' sx={{ textAlign: 'center' }}>
+      }}
+    >
+      <Container maxWidth="md" sx={{ textAlign: "center" }}>
         <Box
           sx={{
             background: `url(${getInTouch})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: '50% 0',
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "50% 0",
             mb: 5,
-            textAlign: 'center',
-          }}>
+            textAlign: "center",
+          }}
+        >
           <Typography
-            variant='h4'
+            variant="h4"
             sx={{
-              textTransform: 'capitalize',
-            }}>
-            Let’s{' '}
-            <Typography variant='h4' component='span' sx={{ color: (theme) => theme.palette.primary.main }}>
+              textTransform: "capitalize",
+            }}
+          >
+            Let’s{" "}
+            <Typography
+              variant="h4"
+              component="span"
+              sx={{ color: (theme) => theme.palette.primary.main }}
+            >
               Talk
             </Typography>
           </Typography>
@@ -37,16 +44,16 @@ export default function GetInTouch() {
           <Divider
             sx={{
               height: 2,
-              background: '#fff',
+              background: "#fff",
               width: 105,
-              mx: 'auto',
+              mx: "auto",
               mt: 1,
             }}
           />
 
-          <Typography variant='h6' sx={{ mt: 4, mb: 8 }}>
-            I'm interested in new opportunities. I'm always open to learn more and collaborate <br /> on projects. Don't
-            hesitate to reach out!
+          <Typography variant="h6" sx={{ mt: 4, mb: 8 }}>
+            I'm always open to learn more and collaborate <br /> on projects.
+            Don't hesitate to reach out!
           </Typography>
           <GetInTouchForm />
         </Box>
