545588c02fd79fa532dd02e7092b6be339cba8ed
alex <alexzelinsky124@gmail.com>
2025-06-23 09:27:33 -0700

    fix spacing on bio

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -76,7 +76,7 @@ export default function About() {
             />
 
             <Typography variant="h5" sx={{ mt: { xs: 3, md: 4 } }}>
-            I’m a full-stack software engineer who turns complex problems into reliable, user-focused web applications.I share my knowledge through clear, practical articles on modern web-development trends, and Dale Carnegie training has honed my ability to communicate technical ideas to any audience. I hold a B.S. in Managerial Economics from UC Davis and am an App Academy graduate.
+            I’m a full-stack software engineer who turns complex problems into reliable, user-focused web applications. I share my knowledge through clear, practical articles on modern web-development trends, and Dale Carnegie training has honed my ability to communicate technical ideas to any audience. I hold a B.S. in Managerial Economics from UC Davis and am an App Academy graduate.
             </Typography>
 
             {/* <Typography variant="h5" sx={{ mt: 1.5 }}>
